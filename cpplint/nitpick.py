f6d4124c04b0d592fbd14148abb2ad31ffd71a1d
Itamar Ostricher <itamarost@gmail.com>
2014-11-10 08:15:24 +0200

    renamed stylify subcommand to style; changed strings to unicode; improved help strings and comments.

--- cpplint/nitpick.py
+++ cpplint/nitpick.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2014 The Ostrich | by Itamar O
 # pylint: disable=protected-access,bad-indentation,too-few-public-methods,global-statement
 
-"""Utility script to automate style stuff."""
+"""Utility script to automate style stuff and other nitpickings."""
 
 __author__ = 'Itamar Ostricher'
 
@@ -19,7 +19,11 @@ import cpplint
 from cpplint import FileInfo
 from cpplint import _ClassifyInclude as classify_include
 
-_STYLER_MODULES = frozenset([u'sort_includes',])
+_STYLE_MODULES_DICT = {
+    u'sort_includes': (u'Automatically divide includes into sections are sort '
+                       'them, according to Google C++ Style Guide'),
+}
+_STYLE_MODULES = frozenset(_STYLE_MODULES_DICT.keys())
 _RE_PATTERN_INCLUDE = re.compile(  # from cpplint + modifications
     r'^\s*#\s*include\s*([<"])\s*([^>"\s]*)\s*[>"](.*$)')
 _SECTIONS_ORDER = [
@@ -34,28 +38,28 @@ _QUIET = False
 def stringify(message, *args):
   """Return formatted message by applying args, if any."""
   if args:
-    return '%s\n' % (message % (args))
+    return u'%s\n' % (message % (args))
   else:
-    return '%s\n' % (message)
+    return u'%s\n' % (message)
 
 def err(message, *args):
   """Error message printer."""
   formatted = stringify(message, *args)
-  sys.stderr.write('ERROR: %s' % (formatted))
+  sys.stderr.write(u'ERROR: %s' % (formatted))
   raise RuntimeError(formatted.strip())
 
 def warn(message, *args):
   """Warning message printer."""
-  sys.stderr.write('WARNING: ' + stringify(message, *args))
+  sys.stderr.write(u'WARNING: ' + stringify(message, *args))
 
 def info(message, *args):
   """Info message printer."""
   if not _QUIET:
-    sys.stderr.write('INFO: ' + stringify(message, *args))
+    sys.stderr.write(u'INFO: ' + stringify(message, *args))
 
 def differ(line):
   """Diff line printer."""
-  sys.stderr.write('%s\n' % (line))
+  sys.stderr.write(u'%s\n' % (line))
 
 class HFile(object):
   """Included h-file class."""
@@ -63,7 +67,7 @@ class HFile(object):
     """Initialize an include file instance from the include string."""
     match = _RE_PATTERN_INCLUDE.match(include_str)
     if not match:
-      raise Exception('Not an include line: "%s"' % (include_str))
+      raise Exception(u'Not an include line: "%s"' % (include_str))
     self.name = match.group(2)
     if match.group(1) == '<':
       self.is_system = True
@@ -128,11 +132,11 @@ def sort_includes(filename, lines):
         # Include repeats in batch
         if repr(hfile) == repr(includes[hkey]):
           # Occurences are consistent - just a warning then
-          warn('"%s" included more than once (consistently) in "%s:%d": %s',
+          warn(u'"%s" included more than once (consistently) in "%s:%d": %s',
                hfile.name, filename, lnum+1, hfile)
         else:
           # Occurences inconsistent! it's an error.
-          err('"%s" included more than once (inconsistently) in "%s:%d": %s',
+          err(u'"%s" included more than once (inconsistently) in "%s:%d": %s',
               hfile.name, filename, lnum+1, hfile)
       else:
         # Add include to batch
@@ -140,11 +144,11 @@ def sort_includes(filename, lines):
         # Sanity check system-vs-project include
         if is_project_file(hfile.name):
           if hfile.is_system:
-            warn('"%s" looks like a project-file, but is included with <> '
+            warn(u'"%s" looks like a project-file, but is included with <> '
                  'in "%s:%d": %s', hfile.name, filename, lnum+1, repr(hfile))
         else:
           if not hfile.is_system:
-            warn('"%s" looks like a system-file, but is included with "" '
+            warn(u'"%s" looks like a system-file, but is included with "" '
                  'in "%s:%d": %s', hfile.name, filename, lnum+1, repr(hfile))
     else:
       if in_batch:
@@ -162,7 +166,7 @@ def sort_includes(filename, lines):
     # In case the source file ends with batch of includes
     new_lines.extend(sort_includes_batch(filename, includes))
   if includes_batches > 1:
-    warn('More than 1 batch of #include\'s in "%s"', filename)
+    warn(u'More than 1 batch of #include\'s in "%s"', filename)
   return new_lines
 
 def stylify_lines(args, filename, lines):
@@ -189,7 +193,7 @@ def stylify_file(args, filepath):
   new_content = u'\n'.join(new_lines)
   if new_content != old_content:
     if args.show_diff:
-      info('Modified content of %s. Diff:', filename)
+      info(u'Modified content of %s. Diff:', filename)
       for diffline in difflib.unified_diff(lines, new_lines,
                                            fromfile='%s (before)' % (filename),
                                            tofile='%s (after)' % (filename)):
@@ -197,50 +201,54 @@ def stylify_file(args, filepath):
     if '-' == filepath:
       sys.stdout.write(new_content)
     elif not args.no_edit:
-      info('Writing changes back to filepath %s ...', filepath)
+      info(u'Writing changes back to filepath %s ...', filepath)
       with codecs.open(filepath, 'w', 'utf8', 'replace') as f_out:
         f_out.write(new_content)
   else:
-    info('No changes for %s ...', filename)
+    info(u'No changes for %s ...', filename)
 
 def stylify(args, files):
   """Stylify files."""
   if args.modules:
     args.modules = set(args.modules)
-    assert args.modules <= _STYLER_MODULES
+    assert args.modules <= _STYLE_MODULES
   else:
-    args.modules = _STYLER_MODULES
+    args.modules = _STYLE_MODULES
   if files:
     for filepath in files:
       filepath = os.path.normpath(filepath)
-      info('Stylifying file %s ...', filepath)
+      info(u'Stylifying file %s ...', filepath)
       try:
         stylify_file(args, filepath)
       except RuntimeError:
-        info('Skipping file %s ...', filepath)
+        info(u'Skipping file %s ...', filepath)
       else:
-        info('Done with file %s ...', filepath)
+        info(u'Done with file %s ...', filepath)
   else:
     stylify_file(args, '-')
 
 def main():
-  """Run neater command on input(s)."""
+  """Run nitpick command on input(s)."""
   parser = argparse.ArgumentParser()
   subparsers = parser.add_subparsers()
-  styler_parser = subparsers.add_parser('stylify',
-                                        help='Stylify C++ source code')
+  styler_parser = subparsers.add_parser('style',
+                                        help=u'Stylify C++ source code')
   styler_parser.add_argument('--no_edit', action='store_true',
-                             help='Don\'t write stylified code back to '
-                             'source file')
+                             help=u'Don\'t overwrite source file with '
+                             'stylified output')
   styler_parser.add_argument('--show_diff', action='store_true',
-                             help='Print diffs between input file and '
+                             help=u'Print diffs between input file and '
                              'stylified file to STDERR')
   styler_parser.add_argument('--quiet', action='store_true',
-                             help='Don\'t print progress '
+                             help=u'Don\'t print progress '
                              '(only warnings and errors)')
-  styler_parser.add_argument('-m', '--modules', action='append')
+  modules_help_str = u'\n'.join([u'%s: %s' % (mod, desc) for mod, desc in
+                                 _STYLE_MODULES_DICT.iteritems()])
+  styler_parser.add_argument('-m', '--modules', action='append', metavar='MOD',
+                             help=modules_help_str)
   styler_parser.add_argument('--filename',
-                             help='Filename in case of STDIN')
+                             help=u'When reading source code from STDIN, spec'
+                             'ify the filename of the processed source code')
   styler_parser.set_defaults(func=stylify)
   # Change stderr to write with replacement characters so we don't die
   # if we try to print something containing non-ASCII characters.
